f5dc0468b6b08910637d3f902d057ea936ba4289
Mateusz Sawicki <mateusz.saw00@wp.pl>
2022-03-08 17:25:53 +0100

    finished modul 5

--- js/script.js
+++ js/script.js
@@ -32,9 +32,47 @@ function titleClickHandler(event){
     correctArticle.classList.add('active');
     
 }
-  
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('link html = ', linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+    }
+    
+    titleList.innerHTML = html;
+
     const links = document.querySelectorAll('.titles a');
-  
+    
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
     }
+
+}
+
+generateTitleLinks();
