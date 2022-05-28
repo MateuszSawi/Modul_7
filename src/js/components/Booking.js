7d563bf0f210134fa22c60dd7bf0bb1804330e0d
Mateusz Sawicki <mateusz.saw00@wp.pl>
2022-05-28 10:49:05 +0200

    ddded

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -2,6 +2,7 @@ import {select, templates, settings} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
+import {utils} from '../utils.js';
 
 class Booking{
   constructor(){
